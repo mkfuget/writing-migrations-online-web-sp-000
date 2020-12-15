7e5a48d923e5358cbabc863260b45aae4c027c81
Michael Fuget <michaelfuget@gmail.com>
2020-12-15 04:06:16 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    change_column :students :birthdate
-    add_column t.integer :grade
-
+    change_column :students :birthdate :datetime
   end
 end
