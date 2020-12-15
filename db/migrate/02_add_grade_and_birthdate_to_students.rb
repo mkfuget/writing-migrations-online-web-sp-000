4b7a36f056b7936709abde89cd2e9763cfee30e5
Michael Fuget <michaelfuget@gmail.com>
2020-12-15 04:03:55 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students do |t|
+    add_column t.string :grade, :birthdate
       t.string :name 
     end 
       
